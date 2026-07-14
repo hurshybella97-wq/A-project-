4181f3ff9172a2de33ab1ab336f432928d5fdb23
hurshybella97-wq <hurshybella97@gmail.com>
2026-07-14 12:56:57 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,15 +1,17 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const registerBtn = document.getElementById("registerBtn");
 
-registerBtn.addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+registerBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    if (!email || !password) {
-        alert("Please enter your email and password.");
-        return;
-    }
-
-    alert("Registration successful!\n\nEmail: " + email);
-
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Registration successful!");
     window.location.href = "index.html";
+  } catch (error) {
+    alert(error.message);
+  }
 });
