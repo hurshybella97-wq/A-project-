4403e0a7a68b255100e7e09086beefb79fb53366
hurshybella97-wq <hurshybella97@gmail.com>
2026-07-14 12:58:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const loginBtn = document.getElementById("loginBtn");
 
-loginBtn.addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+loginBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    if (!email || !password) {
-        alert("Please enter your email and password.");
-        return;
-    }
-
-    alert("Login successful!\n\nEmail: " + email);
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login successful!");
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
 });
